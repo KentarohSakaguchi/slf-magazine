7b7af96c2194e587881e1352b33c3d1ee9707024
Chamado <chamado.office@gmail.com>
2018-08-30 00:22:56 +0900

    POSTのデータをJSON変換

--- config/Post/EditPost.js
+++ config/Post/EditPost.js
@@ -25,28 +25,31 @@ let posteadHtml;
  */
 const EditPost = (req, res) => {
 
-  let body = '';
+  return new Promise((resolve, reject) => {
 
-  req.on('data', (chunk) => {
-    body += chunk;
+    let body = '';
+
+    req.on('data', (chunk) => {
+      body += chunk;
+    });
+
+    req.on('end', () => {
+
+      posteadHtml = qs.parse(body);
+      pugOptions.postHtml = posteadHtml.save;
+      const dataJson = JSON.stringify(posteadHtml);
+
+      const renderPug = fs.readFileSync(`${Config.HTML_PATH}/result.pug`, 'utf8');
+      pugOptions.filename = `${Config.HTML_PATH}/result.pug`;
+      const content = pug.render(renderPug, pugOptions);
+
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(content);
+      res.end();
+
+      resolve(dataJson);
+    });
   });
-
-  req.on('end', () => {
-
-    posteadHtml = qs.parse(body);
-    pugOptions.postHtml = posteadHtml.save;
-    console.log(posteadHtml);
-
-    const renderPug = fs.readFileSync(`${Config.HTML_PATH}/result.pug`, 'utf8');
-    pugOptions.filename = `${Config.HTML_PATH}/result.pug`;
-    const content = pug.render(renderPug, pugOptions);
-
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(content);
-    res.end();
-    return posteadHtml;
-  });
-
 };
 
 module.exports = {
