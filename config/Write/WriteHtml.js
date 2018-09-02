977ec127213bdd844b970868dff18962bc753c7b
Chamado <chamado.office@gmail.com>
2018-09-02 22:32:52 +0900

    render html調整

--- config/Write/WriteHtml.js
+++ config/Write/WriteHtml.js
@@ -14,9 +14,7 @@ const Filelist = require('../Config/Filelist');
 // pugの設定情報
 const pugOptions = {
   pretty: true,
-  locals: {
-    file: ''
-  }
+  saverText: ''
 };
 
 /**
@@ -28,29 +26,31 @@ const pugOptions = {
 const WriteHtml = (res, url_parse) => {
 
   let fileName = url_parse.pathname.replace('/', '').replace(/.*?\//, ''); // url情報取得
+  const viewList = glob.sync(`${Config.HTML_PATH}/!(_|record)*`); // viewファイル取得
 
-  if (url_parse.pathname === '/') {
+  let recordFlg = true; // record.pugに書き込みOK flg
+
+  for (data of viewList) {
+
+    const view = data.replace(`${Config.HTML_PATH}/`, '').replace('.pug', '');
+    if (fileName === view) {
+      recordFlg = false;
+      break;
+    }
+  }
+
+  if (url_parse.pathname === '/' && recordFlg) {
     // index ページ
     fileName = 'index'; // url情報取得
 
-  } else {
-
-    // その他ページ
-    const viewList = glob.sync(`${Config.HTML_PATH}/!(_)*`);
-
-    // 404のち
-    for (data of viewList) {
-      const view = data.replace(Config.HTML_PATH, '').replace('.pug', '');
-      if (fileName !== view) {
-        pugOptions.locals.file = fileName;
-        fileName = 'record';
-      }
-    }
+  } else if (recordFlg){
+    fileName = 'record';
   }
 
   const fileIndex = Filelist.HTML_LIST.indexOf(fileName);
   const renderPug = fs.readFileSync(`${Config.HTML_PATH}/${Filelist.HTML_LIST[fileIndex]}.pug`, 'utf8');
   pugOptions.filename = `${Config.HTML_PATH}/${Filelist.HTML_LIST[fileIndex]}.pug`;
+  pugOptions.saverText = url_parse.pathname.replace(/.*?\//, '');
 
   const content = pug.render(renderPug, pugOptions);
 
