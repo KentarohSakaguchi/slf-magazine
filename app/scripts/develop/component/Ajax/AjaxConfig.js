eb5c79e4d4e007e54d58686a6af0142820d57a98
Chamado <chamado.office@gmail.com>
2018-09-05 23:31:23 +0900

    調整

---
+++ app/scripts/develop/component/Ajax/AjaxConfig.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+/**
+ * ajaxの設定クラス → Ajax.jsのみでインスタンスを作る
+ */
+class AjaxConfig{
+
+  constructor(){
+    this.url = null;
+    this.param = null;
+  }
+
+  get getAjax(){
+
+    /* eslint no-undef: 0 */
+    return new Promise((resolve, reject) => {
+
+      axios
+        .get(this.url, { params: this.param })
+        .then((results) => {
+          resolve(results);
+        })
+        .catch((results) => {
+          console.error(results);
+          reject(results);
+        });
+    });
+  }
+
+  set setUrl(url){
+    this.url = url;
+  }
+
+  set setAjax(param){
+    console.log(param);
+    this.param = param;
+  }
+
+}
+
+export default AjaxConfig;
---
+++ app/scripts/develop/component/RecordPage/RecordPage.jsx
@@ -0,0 +1,46 @@
+/**
+ * indexページの処理
+ */
+
+import React, { Component } from 'react';
+import { Ajax, GetAjax, SetAjax } from '../Ajax/Ajax';
+
+class RecordPage extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.dataArray = [];
+    this.state = {
+      data: this.dataArray
+    };
+
+    // 初回読み込み
+    Ajax('/record/list');
+    const setPram = location.pathname.replace(/\//g, '');
+    SetAjax(`report=${setPram}`);
+
+    GetAjax().then((result) => { 
+      console.log(result.data);
+      this.setState({
+        data: result.data
+      });
+    });
+
+  }
+
+  renderHtml() {
+    console.log(this.state.data)
+    return this.state.data.map((value) => {
+      return value.json.save;
+    })
+  }
+  
+
+  render() {
+    console.log(this.state.data);
+    return <div className="report" dangerouslySetInnerHTML={{__html: this.renderHtml()}}></div>
+  }
+}
+
+export default RecordPage;
