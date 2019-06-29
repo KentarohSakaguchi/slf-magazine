b24a3419eda0c613cb37abe3623a008cdfecd919
KentarohSakaguchi <sakaguchi1000m@gmail.com>
2019-06-29 21:14:33 +0900

    差分修正
    Signed-off-by: Chamado <chamado.office@gmail.com>

---
+++ app/scripts/develop/component/Edit/component/EditSelectLang.jsx
@@ -0,0 +1,70 @@
+/**
+ * @file 言語フォーム(選択型)
+ * 
+ */
+
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class EditSelectLang extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  /**
+   * セレクトボックス
+   */
+  handleChange(setValue) {
+    return this.props.selectLangValueChange(setValue)
+  }
+
+  setOption() {
+    const items = this.props.selectLangList.map((_item, _number) => (
+      <option key={_number} value={_item}>{_item}</option>
+    ));
+
+    return items;
+  }
+
+  /**
+   * input
+   */
+  valueChange(setValue) {
+    return this.props.textLangValueChange(setValue);
+  }
+
+  render() {
+    this.setOption();
+    return(
+      <div className="edit__input-block">
+        <label className="edit__input-title edit__input-title--lang">
+          <select
+            className="edit__input-select"
+            name="lang"
+            value={this.props.selectLangValue}
+            onChange={(e) => this.handleChange(e.target.value)}>
+            {this.setOption()}
+          </select>
+        </label>
+        <label className="edit__input-input">
+        <input className="edit__input" type="text" name="word" placeholder="関連ワードを入力" value={this.props.textLangValue} onChange={(e) => this.valueChange(e.target.value)} />
+        </label>
+      </div>
+    );
+  }
+
+}
+
+EditSelectLang.propTypes = {
+  selectValue: PropTypes.func,
+  selectLangValueChange: PropTypes.func,
+  selectLangValue: PropTypes.string,
+  selectLangList: PropTypes.array,
+  textLangValueChange: PropTypes.func,
+  textLangValue: PropTypes.string
+}
+
+export default EditSelectLang;
---
+++ app/scripts/develop/component/Submit/Submit.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+
+/**
+ * Submitページの処理
+ */
+class Submit extends Component {
+
+  constructor(props) {
+
+    super(props);
+
+    this.onRec = this.onRec.bind(this);
+    this.cancel = this.cancel.bind(this);
+
+    this.state = {
+      submitClass: ''
+    };
+
+    this.addFlg = false;
+  }
+
+  /**
+   * 記録ボタン
+   */
+  onRec() {
+
+    if (this.state.submitClass !== 'is-show') {
+      this.setState({ submitClass: 'is-show'});
+    }
+  }
+
+  cancel() {
+    if (this.state.submitClass === 'is-show') {
+      this.setState({ submitClass: ''});
+    }
+  }
+
+  render() {
+    return(
+      <div className="edit-wrapper">
+        <div className="edit result">
+          <div className="edit__button-wrapper">
+            <button
+              type="button"
+              className="edit__button edit__button--rec"
+              onClick={this.onRec}>
+            </button>
+          </div>
+          <div className={`edit__button-wrapper edit__button-wrapper--submit ${this.state.submitClass}`}>
+            <div className="button-wrapper">
+              <button
+                type="submit"
+                className="button button--submit">
+                OK
+              </button>
+              <a
+                href="/edit?back=cancel"
+                className="button button--ng"
+                onClick={this.cancel}>
+                CANCEL
+              </a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+
+}
+
+
+
+export default Submit;
